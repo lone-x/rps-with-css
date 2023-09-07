eeffd83221ebb3c497140569d0e97e02f5031cf9
nashwan <lonexnashwan@gmail.com>
2023-09-07 19:55:59 +0530

    autoplay

--- script.js
+++ script.js
@@ -16,6 +16,21 @@
       }
   */
 
+      let isAutoPlaying =false;
+      let intervalId;
+   function autoplay(){
+      if(!isAutoPlaying){
+           intervalId= setInterval(function(){
+                  const playerMove=pickComputerMove();
+                  playGame(playerMove)
+            },1000);
+            isAutoPlaying=true;
+      }else{
+            clearInterval(intervalId);
+            isAutoPlaying=false;
+      }
+   }
+
   function playGame(playerMove) {
         const computerMove = pickComputerMove();
 
